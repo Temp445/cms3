1ffdf9c1131082f678b2b8d2a3e6dbb91cad0771
Temp445 <acetemp345@gmail.com>
2025-05-21 16:03:53 +0530

    first commit

---
+++ frontend/src/components/Hero.tsx
@@ -0,0 +1,65 @@
+
+import Navbar from "./Navbar";
+import logo from "../assets/AceLogo.png";
+import Image from "next/image";
+import Link from "next/link";
+
+const Hero = () => {
+
+
+  return (
+    <div className="bg-slate-100 relative min-h-fit overflow-hidden " id="top">
+      <div className="absolute inset-0 bg-gradient-to-br from-gray-100 via-gray-100  z-0"></div>
+
+      <div className="relative min-h-fit overflow-hidden bg-slate-100 container mx-auto ">
+        <div className="absolute inset-0 bg-gradient-to-br from-gray-100 via-gray-100 z-0"></div>
+
+        <div className="absolute top-20 right-40 w-64 h-64 rounded-full bg-indigo-400 opacity-80 blur-lg z-0 animate-pulse "></div>
+        <div className="flex absolute top-16 left-5 md:top-40 md:left-10 w-10 h-10 md:w-16 md:h-16  border border-red-600 transform rotate-45 opacity-70 z-0 rectangle"></div>
+        <div className="flex absolute bottom-20 left-80 md:left-20 md:bottom-32 lg:bottom-20 lg:left-40 w-32 h-8 rounded-full bg-teal-400 opacity-60 z-0"></div>
+        <div className="flex absolute right-3 bottom-5 md:right-10 md:bottom-10 lg:bottom-20 lg:right-20 w-16 h-1 md:w-20 md:h-20 lg:w-24 lg:h-24 rounded-full bg-violet-400 opacity-80 z-0"></div>
+        <div className="flex absolute top-60  right-20 md:top-32 md:right-28 w-4 h-4 rounded-full border border-teal-500 z-0"></div>
+        <div className="hidden lg:flex absolute bottom-60 left-20 w-32 h-12 rounded-full bg-indigo-300 opacity-70 z-0"></div>
+        <div className="flex absolute top-80 left-3 md:top-96 md:left-56 w-5 h-5 md:w-12 md:h-12 rounded-full bg-indigo-400 opacity-80 z-0"></div>
+        <div className="hidden lg:flex absolute bottom-20 right-80 lg:right-60 w-10 h-10 lg:w-16 lg:h-16 rounded-full bg-violet-500 opacity-70 z-0"></div>
+        <div className="flex absolute right-3 bottom-44 md:right-10 md:bottom-36 lg:right-20 lg:bottom-52 w-10 h-10 md:w-10 md:h-10 lg:w-16 lg:h-16 rounded-full bg-indigo-500 opacity-70 z-0"></div>
+
+        <div className="relative z-10 max-w-7xl mx-auto px-4 sm:px-6 container -mt-12 pb-5 md:mt-2 ">
+          <div className="">
+            {" "}
+            <Navbar />{" "}
+          </div>
+          <div className="md:hidden flex gap-1 mx-auto justify-center w-full py-3 -ml-2">
+            <Image src={logo} alt="logo" className="w-8 h-8" />
+            <span className="text-base font-semibold mt-1 text-gray-700">
+              ACE CMS
+            </span>
+          </div>
+
+          <div className=" py-0 pb-5 md:py-20 lg:py-24 text-center">
+            <h1 className="text-2xl md:text-4xl lg:text-5xl xl:text-6xl font-bold text-gray-900 mb-6">
+              Streamline Your Calibration Processes with{" "}
+              <span className="">
+                ACE CMS
+              </span>
+            </h1>
+            <p className="text-lg text-gray-600 max-w-2xl mx-auto">
+              Efficient, compliant, and cloud-based calibration management
+              tailored for manufacturing excellence.
+            </p>
+
+            <div className="mt-10 ">
+              <Link href="#contact"
+                className="border border-[#071952] bg-[#071952] text-white p-2 md:px-10 md:py-3  rounded-md font-semibold  transition"
+              >
+                Contact Us
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
---
+++ frontend/src/components/Scenario.tsx
@@ -0,0 +1,38 @@
+
+import Image from 'next/image';
+import React from 'react';
+import cms from '../assets/CMS1.png'
+import Link from 'next/link';
+
+
+const Scenario = () => {
+
+
+  return (
+    <div className="flex w-full min-h-fit bg-slate-200 items-center justify-center py-12 px-4 sm:px-6 lg:px-20 mt-2  md:mt-10">
+      <div className="bg-white rounded-2xl shadow-xl w-full max-w-6xl p-4 sm:p-8 md:p-12 flex flex-col lg:flex-row items-center md:items-start relative">
+        <div className='hidden w-32 h-3 inset-0 lg:flex mx-auto absolute bg-[#071952]  rounded-b-lg '></div>
+        <div className="lg:w-1/2 mb-6 md:mb-0 md:pr-8 order-2 lg:order-1">
+          <h1 className="text-2xl sm:text-4xl lg:text-3xl xl:text-5xl font-bold mb-4 text-gray-800">
+            ACE Calibration Management System
+          </h1>
+          <p className=" md:text-lg text-justify sm:text-xl lg:text-sm xl:text-xl text-gray-600 leading-relaxed md:mt-5">
+            Imagine your next audit — no more paper trails, no more scrambling for certificates. 
+            With <span className="font-bold ">ACE CMS</span>, everything you need is ready at a click, 
+            and your auditors are impressed. Your team stays calm, focused, and confident that everything is in order.
+          </p>
+<div className='mt-10'>
+          <Link href="#contact"  className='mt-6 p-2 rounded bg-[#071952] text-white md:text-lg lg:text-sm xl:text-lg font-semibold '>Book A Demo</Link>
+
+</div>
+        </div>
+
+        <div className="mb-5 lg:w-1/2 order-1 md:mb-0 lg:order-2 md:pb-10">
+     <Image src={cms} alt="cms" className='rounded-lg md:shadow-xl shadow-indigo-300' />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Scenario;
